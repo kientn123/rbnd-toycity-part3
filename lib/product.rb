e411b48887e297e9d5a0a24dc5b6e73c0854f5cf
Kien Nguyen <kiennt.sic@gmail.com>
2016-04-08 12:10:25 -0500

    handle duplicate errors

--- lib/product.rb
+++ lib/product.rb
@@ -1,13 +1,25 @@
 class Product
 
+  attr_reader :title
+
   @@products = []
 
-  def initialization(options={})
-    @@products << self
+  def initialize(options={})
+    @title = options[:title]
+    add_to_products
   end
 
   def self.all
     @@products
+  end
+
+  def add_to_products
+    @@products.each do |product|
+      if product.title == @title
+        raise DuplicateProductError.new("#{@title} already exists.")
+      end
+    end
+    @@products << self
   end
 
 end
